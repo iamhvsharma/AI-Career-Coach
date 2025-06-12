d88b089ea52bc2e9d7920b15ae20ff0513873952
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-06-12 11:54:34 +0530

    update

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -75,7 +75,7 @@ export function AppSidebar() {
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
+                <h2 className='p-2 text-gray-400 text-sm'>Copyright to @Harshvardhan</h2>
             </SidebarFooter>
         </Sidebar>
     )
