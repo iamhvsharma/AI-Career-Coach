ef85f3805c961064cdc48d752f757bbb04181da9
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-06-14 15:20:55 +0530

    Add SVG files for cover letter, Q&A, and resume icons
    - Created cover-letter.svg with a design representing a cover letter.
    - Added qa.svg for a web Q&A icon.
    - Introduced resume.svg featuring a resume design.

--- app/(routes)/dashboard/_components/AiToolsCard.tsx
+++ app/(routes)/dashboard/_components/AiToolsCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -17,7 +18,7 @@ export type AIToolsProps = {
 const AiToolsCard = ({ tool }: AIToolsProps) => {
   return (
     <div className="p-3 border rounded-lg">
-      {/* <Image src={tool.icon} width={40} height={40} alt={tool.name} />  */}
+      <Image src={tool.icon} width={40} height={40} alt={tool.name} /> 
       <h2 className="font-medium mt-2">{tool.name}</h2>
       <p className="text-gray-400">{tool.desc}</p>
       <Link href={tool.path}>
